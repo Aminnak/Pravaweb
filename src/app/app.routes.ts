9ca37930653a9135a7fd082fd6ad4a33934dba6e
aminnak <aminamirikeivani@gmail.com>
2024-12-15 01:13:49 +0330

    Initial commit with Angular setup + the root component style + some path config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./Components/home/home.component";
+import { ServicesComponent } from './Components/service-component/services.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path : '' , redirectTo : '/home' , pathMatch : 'full'},
+    {path : 'home' , component : HomeComponent},
+    {path : 'services' , component : ServicesComponent},
+    {path : 'sign-in' , component : SignInComponent},
+];
