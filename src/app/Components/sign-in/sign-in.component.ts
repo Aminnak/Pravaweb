49a28210d827fc106a0007532d19041185b2b4cb
aminnak <aminamirikeivani@gmail.com>
2024-12-18 02:22:54 +0330

    Form template and some minor changes in other files added

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule , FormBuilder ,FormGroup , Validators} from "@angular/forms";
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [ReactiveFormsModule , CommonModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-
+    Form : FormGroup;
+    tempFormError :Boolean = false
+    constructor(private formBuilder : FormBuilder){
+        this.Form = this.formBuilder.group({
+            username : ['',[Validators.required]],
+            password : ['',[Validators.required]],
+            confirmPassword : ['',[Validators.required]],
+            postalCode : [''],
+            email : ['',[Validators.required]],
+            phoneNumber : ['']
+        })
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet , RouterModule} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet , RouterModule ,Router , NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { RouterOutlet , RouterModule} from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Angular-Tailwind';
+export class AppComponent implements OnInit{
+    currentRoute : string = '';
+
+    constructor(private router : Router){}
+
+    ngOnInit(): void {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(() => {
+            this.currentRoute = this.router.url
+        })
+    }
 }
