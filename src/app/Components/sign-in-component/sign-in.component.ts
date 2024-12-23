949ddab0ef3b9083dd11bdd8f757f774aa8a4d16
aminnak <aminamirikeivani@gmail.com>
2024-12-24 02:34:37 +0330

    Register form templated fixed & all the validations are done and set

--- src/app/Components/sign-in-component/sign-in.component.ts
+++ src/app/Components/sign-in-component/sign-in.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule , FormBuilder ,FormGroup , Validators} from "@angular/forms";
-import { lowercaseValidator , uppercaseValidator} from '../../shared/Validations';
+import { ReactiveFormsModule , FormBuilder ,FormGroup , Validators, AbstractControlOptions} from "@angular/forms";
+import { lowercaseValidator , uppercaseValidator , passValidator , passMatch} from '../../shared/Validations';
 @Component({
   selector: 'app-sign-in',
   imports: [ReactiveFormsModule , CommonModule],
@@ -9,16 +9,22 @@ import { lowercaseValidator , uppercaseValidator} from '../../shared/Validations
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+    isSumbitActive = false
     Form : FormGroup;
     tempFormError :Boolean = false
     constructor(private formBuilder : FormBuilder){
-        this.Form = this.formBuilder.group({
-            username : ['',[Validators.required,lowercaseValidator,uppercaseValidator]],
-            password : ['',[Validators.required]],
+        this.Form = this.formBuilder.group(
+            {
+            username : ['',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=(.*[A-Za-z]{4})[A-Za-z0-9])/)]],
+            password : ['',[Validators.required,passValidator]],
             confirmPassword : ['',[Validators.required]],
-            postalCode : [''],
-            email : ['',[Validators.required]],
-            phoneNumber : ['',]
-        })
+            postalCode : ['',[Validators.pattern(/^\d{10}$/)]],
+            email : ['',[Validators.required,Validators.email]],
+            phoneNumber : ['',[Validators.pattern(/^09\d{9}$/)]]
+        },
+        {
+            validators : passMatch()
+        } as AbstractControlOptions
+    )
     }
 }
--- src/app/shared/Validations.ts
+++ src/app/shared/Validations.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function lowercaseValidator(control:AbstractControl):ValidationErrors | null{
     const value = control.value || ''
@@ -8,4 +8,18 @@ export function lowercaseValidator(control:AbstractControl):ValidationErrors | n
 export function uppercaseValidator(control:AbstractControl) : ValidationErrors | null {
     const value = control.value || ''
     return /[A-Z]/.test(value) ? null : {uppercaseValidator : 'should at least contains one uppercase char'}
+}
+
+
+export function passValidator(control : AbstractControl) : ValidationErrors | null {
+    const value = control.value || ''
+    return /^(?=(.*[!@?%#$^]))(?=(.*\d.*.\d.))(?=(.*[A-Z]))[A-Za-z\d@!?%#$^]*$/.test(value) ? null : { passwordValidator : 'Please enter a strong '}
+}
+
+export function passMatch() : ValidatorFn {
+    return (control : AbstractControl) : ValidationErrors | null  => {
+        const Password = control.get('password')?.value
+        const ConfirmPassword = control.get('confirmPassword')?.value
+        return Password === ConfirmPassword ? null : {passError : 'Password and Confirm Password didnt matched'}
+    }
 }
